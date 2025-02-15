abff20c16b900aeb9e252120bdb7a08b4370a1ef
matheustorresdev97 <matheustorresdev@gmail.com>
2025-02-15 19:40:35 -0300

    obtendo a moeda selecionada e o evento de submit

--- 01-convert/script.js
+++ 01-convert/script.js
@@ -1,6 +1,13 @@
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
 
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g;
   amount.value = amount.value.replace(hasCharactersRegex, "");
 });
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  console.log(currency.value);
+};
