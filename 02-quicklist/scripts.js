ec14176f3e248c351c1edc9d46ada8878b42cf6b
matheustorresdev97 <matheustorresdev@gmail.com>
2025-02-15 20:11:10 -0300

    add novo item

---
+++ 02-quicklist/scripts.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("form");
+const newItem = document.getElementById("new-item");
+const button = document.querySelector('button[type="submit"]');
+const itemList = document.getElementById("items-list");
+const item = document.getElementsByClassName("item");
+const checkbox = document.querySelector('input[type="checkbox"]');
+const itemName = document.querySelector("span");
+const removeButton = document.querySelector("li > svg");
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  addItem(newItem.value);
+
+  button.blur();
+};
+
+function addItem(name) {
+  const newItem = document.createElement("li");
+  newItem.setAttribute("class", "item");
+
+  const new_checkbox = document.createElement("input");
+  new_checkbox.setAttribute("type", "checkbox");
+  newItem.append(new_checkbox);
+  const newName = document.createElement("span");
+  newName.textContent = name;
+  newItem.append(newName);
+  const newRemoveImg = document.createElement("img");
+  newRemoveImg.setAttribute("src", "./img/trash-bin.svg");
+  newRemoveImg.setAttribute("alt", "Ícone de lata de lixo");
+  newItem.append(newRemoveImg);
+  itemList.prepend(newItem);
+}
