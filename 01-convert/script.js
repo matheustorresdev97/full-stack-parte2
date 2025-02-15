b074bdeb7b30f25f2b626e73eb1e7a7a2a39c346
matheustorresdev97 <matheustorresdev@gmail.com>
2025-02-15 19:44:53 -0300

    cria a fc para formatar a moeda

--- 01-convert/script.js
+++ 01-convert/script.js
@@ -5,6 +5,8 @@ const GBP = 7.43;
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
 
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g;
@@ -28,5 +30,22 @@ form.onsubmit = (event) => {
 };
 
 function convertCurrency(amount, price, symbol) {
-  console.log(amount, price, symbol);
+  try {
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+    footer.classList.add("show-result");
+  } catch {
+    // Remove a classe do footer removendo ele da tela.
+    footer.classList.remove("show-result");
+
+    console.log(error);
+    alert("Não foi possível converter. Tente novamente mais tarde.");
+  }
 }
+
+function formatCurrencyBRL(value) {
+    return Number(value).toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    })
+  }
