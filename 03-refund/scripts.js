bf680553e8f68264f203c7a83bdecb55adfcb215
matheustorresdev97 <matheustorresdev@gmail.com>
2025-02-21 09:35:36 -0300

    formatando o valor total das despesas

--- 03-refund/scripts.js
+++ 03-refund/scripts.js
@@ -92,7 +92,7 @@ function updateTotals() {
         for (let item = 0; item < items.length; item++) {
             const itemAmount = items[item].querySelector(".expense-amount")
 
-            let value = itemAmount.textContent.replace(/[^\d]/g, "").replace(",", ".")
+            let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",", ".")
 
             value = parseFloat(value)
 
@@ -103,7 +103,15 @@ function updateTotals() {
             total += Number(value)
         }
 
-        expensesTotal.textContent = total
+        const symbolBRL = document.createElement("small")
+        symbolBRL.textContent = "R$"
+
+        total = formatCurrencyBRL(total).toUpperCase().replace("R$", "")
+
+
+        expensesTotal.innerHTML = ""
+
+        expensesTotal.append(symbolBRL, total)
     } catch (error) {
         alert("Não foi possível atualizar os totais.")
         console.log(error)
