7aa841eb913a82f006eecf5b93ccb18021729c32
matheustorresdev97 <matheustorresdev@gmail.com>
2025-02-26 19:19:02 -0300

    sections done

--- 06-agendamento-petshop/src/modules/header-date.js
+++ 06-agendamento-petshop/src/modules/header-date.js
@@ -3,8 +3,8 @@ import dayjs from "dayjs";
 const textDate = document.getElementById("text-date");
 const inputDate = document.getElementById("date");
 
-textDate.textContent = dayjs().format("DD/MM/YYYY");
+textDate.textContent = dayjs().format("MMM DD, YYYY");
 inputDate.min = dayjs().format("YYYY-MM-DD");
 
 inputDate.onchange = () =>
-  (textDate.textContent = dayjs(inputDate.value).format("DD/MM/YYYY"));
+  (textDate.textContent = dayjs(inputDate.value).format("MMM DD, YYYY"));
