34b0c29cc4704f1cc477759eb226e5b7095c3f02
matheustorresdev97 <matheustorresdev@gmail.com>
2025-09-09 18:40:50 -0300

    criando novo item e adicionando

--- quicklist/script.js
+++ quicklist/script.js
@@ -3,3 +3,52 @@ const addItemButton = document.querySelector("#add-item-btn");
 const itemList = document.querySelector("#item-list");
 const alert = document.querySelector(".alert");
 const closeAlertButton = document.querySelector(".close-alert-btn");
+
+function createListItem(itemText) {
+  const newItem = document.createElement("li");
+
+  const checkboxWrapper = document.createElement("div");
+  checkboxWrapper.classList.add("checkbox-wrapper");
+
+  const label = document.createElement("label");
+  const checkboxImg = document.createElement("div");
+  checkboxImg.classList.add("checkbox-img");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  label.appendChild(checkboxImg);
+  label.appendChild(checkbox);
+  checkboxWrapper.appendChild(label);
+
+  const itemName = document.createElement("span");
+  itemName.textContent = itemText;
+
+  const deleteButton = document.createElement("img");
+  deleteButton.classList.add("delete-btn");
+  deleteButton.src = "assets/trash-can.svg";
+
+  newItem.appendChild(checkboxWrapper);
+  newItem.appendChild(itemName);
+  newItem.appendChild(deleteButton);
+
+  return newItem;
+}
+
+function addItem() {
+  const itemText = inputItem.value.trim();
+
+  if (itemText) {
+    const newItem = createListItem(itemText);
+    itemList.appendChild(newItem);
+    inputItem.value = "";
+  }
+}
+
+addItemButton.addEventListener("click", addItem);
+inputItem.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    addItem();
+  }
+});
