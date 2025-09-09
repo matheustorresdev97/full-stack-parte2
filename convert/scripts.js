a13052a13baefe597a0f83f784d070930f5232db
matheustorresdev97 <matheustorresdev@gmail.com>
2025-09-09 18:27:48 -0300

    exibindo e formatando o total

--- convert/scripts.js
+++ convert/scripts.js
@@ -7,6 +7,7 @@ const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
 const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g;
@@ -32,6 +33,16 @@ form.onsubmit = (event) => {
 function convertCurrency(amount, price, symbol) {
   try {
     description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+    let total = amount * price;
+
+    if (isNaN(total)) {
+      return alert("Por favor, digite o valor corretamente para converter.");
+    }
+
+    total = formatCurrencyBRL(total).replace("R$", "");
+
+    result.textContent = `${total} Reais`;
 
     footer.classList.add("show-result");
   } catch {
