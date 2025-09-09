3aefec99ff0b800f76b65fdc04415fd8ab69f58f
matheustorresdev97 <matheustorresdev@gmail.com>
2025-09-09 18:40:12 -0300

    selecionando itens

---
+++ quicklist/script.js
@@ -0,0 +1,5 @@
+const inputItem = document.querySelector("#input-item");
+const addItemButton = document.querySelector("#add-item-btn");
+const itemList = document.querySelector("#item-list");
+const alert = document.querySelector(".alert");
+const closeAlertButton = document.querySelector(".close-alert-btn");
