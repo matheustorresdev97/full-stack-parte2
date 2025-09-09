245c2df6943bdc0eb0937ac5e8feaeaacf720efb
matheustorresdev97 <matheustorresdev@gmail.com>
2025-09-09 18:23:43 -0300

    identificando a moeda

--- convert/scripts.js
+++ convert/scripts.js
@@ -1,3 +1,7 @@
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
+
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
@@ -10,5 +14,19 @@ amount.addEventListener("input", () => {
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  console.log(currency.value);
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+      break;
+  }
 };
+
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol);
+}
