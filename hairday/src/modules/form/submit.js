55d345f6324352628d5d0eb80c5b7d5675ceea34
matheustorresdev97 <matheustorresdev@gmail.com>
2025-09-10 20:46:00 -0300

    preparando o objeto para ser inserido no agendamento

--- hairday/src/modules/form/submit.js
+++ hairday/src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector("form");
+const clientName = document.getElementById("client");
 const selectedDate = document.getElementById("date");
 
 const dateToday = dayjs(new Date()).format("YYYY-MM-DD");
@@ -10,4 +11,27 @@ selectedDate.min = dateToday;
 
 form.onsubmit = (event) => {
   event.preventDefault();
+
+  try {
+    const name = clientName.value.trim();
+
+    if (!name) {
+      return alert("Informe o nome do cliente!");
+    }
+
+    const hourSelected = document.querySelector(".hour-selected");
+
+    if (!hourSelected) {
+      return alert("Selecione a hora.");
+    }
+
+    const [hour] = hourSelected.innerText.split(":");
+
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+
+    const id = new Date().getTime();
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento.");
+    console.log(error);
+  }
 };
