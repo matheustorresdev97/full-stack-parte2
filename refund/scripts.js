404186b338dee13cb20a10d2736d9a4580c95775
matheustorresdev97 <matheustorresdev@gmail.com>
2025-09-10 09:52:58 -0300

    calc o valor total das despesas

--- refund/scripts.js
+++ refund/scripts.js
@@ -4,6 +4,7 @@ const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
 const expenseList = document.querySelector("ul")
+const expensesTotal = document.querySelector("aside header h2")
 const expenseQuantity = document.querySelector("aside header p span")
 
 amount.oninput = () => {
@@ -84,6 +85,24 @@ function updateTotals() {
         const items = expenseList.children
 
         expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+        let total = 0
+
+        for (let item = 0; item < items.length; item++) {
+            const itemAmount = items[item].querySelector(".expense-amount")
+
+            let value = itemAmount.textContent.replace(/[^\d]/g, "").replace(",", ".")
+
+            value = parseFloat(value)
+
+            if (isNaN(value)) {
+                return alert("Não foi possível calcular o total. O valor não parece ser um número.")
+            }
+
+            total += Number(value)
+        }
+
+        expensesTotal.textContent = total
 
     } catch (error) {
         alert("Não foi possível atualizar os totais.")
