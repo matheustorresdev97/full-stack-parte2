a9b82bcc70c620ca5b4aea282638a67f6eff90aa
matheustorresdev97 <matheustorresdev@gmail.com>
2025-09-11 11:05:16 -0300

    config: webpack babel e dayjs

---
+++ agendamento-petshop/src/scripts/main.js
@@ -0,0 +1,9 @@
+"use strict"
+
+import "../lib/dayjs.js"
+
+import "../styles/global.css"
+import "../styles/layout.css"
+import "../styles/overlay.css"
+import "../styles/periods.css"
+import "../styles/utilities.css"
